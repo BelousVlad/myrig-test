962e69ed6b7f309971ae86f77f2385b0537e094a
Vlad Belous <chebureck333@gmail.com>
2021-08-18 16:15:09 +0300

    tests

--- tests/Feature/EmployeeTest.php
+++ tests/Feature/EmployeeTest.php
@@ -9,15 +9,31 @@ use App\Models\Employee;
 
 class EmployeeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_delete_employee_get_json()
     {
         $employee = Employee::factory()->create();
         $response = $this->delete('api/employees/'.$employee->id);
+        //Удалён ли
         $this->assertDeleted($employee);
+        //Тот ли json пришёл
+        $response
+            ->assertJson(fn ($json) =>
+                $json->where('id', $employee->id)
+                    ->where('first_name', $employee->first_name)
+                    ->where('last_name', $employee->last_name)
+                    ->where('date_of_birth', $employee->date_of_birth)
+                    ->where('weight', $employee->weight)
+                    ->where('height', $employee->height)
+                    ->where('salary', $employee->salary)
+                    ->where('position_id', $employee->position_id)
+                    ->etc()
+            );
     }
+
+    public function test_delete_employee_incorrect_id()
+    {
+        $response = $this->delete('api/employees/-1');
+        $response->assertStatus(404);
+    }
+
 }
