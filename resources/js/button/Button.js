36c773a37257c035f52939e1e95cd081be4a0349
Vlad Belous <chebureck333@gmail.com>
2021-08-20 15:52:23 +0300

    button box-shadow

--- resources/js/button/Button.js
+++ resources/js/button/Button.js
@@ -12,15 +12,19 @@ const StyledButton = styled.button`
     border: none;
     outline: none;
 
+    box-shadow: 0px 6px 24px -12px rgba(0, 128, 73, 0.54);
     
     &:hover {
         background: #60BE96;
+        box-shadow: 0px 12px 36px -12px rgba(0, 128, 73, 0.54);
     }
     &:active {
         background: #3F8D6C;
+        box-shadow: 0px 6px 24px -12px rgba(0, 128, 73, 0.54);
     }
     &:disabled {
         background: #C9C9C9;
+        box-shadow: none;
     }
 `
 
